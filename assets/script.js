b804e119d8fe64892ad976f5408d76c816089000
wjimh314 <wjimh314@gmail.com>
2022-07-21 17:52:55 -0400

    more edits to javascript file

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,15 @@ var confirmLower = "abcdefghijklmnopqrstuvwxyz";
 var confirmUpper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var optionsNumber="8-126";
 var ConfirmSymbols="!@#$%^&*_-+=";
-
+var confirmLength, confirmLength, confirmUpper, confirmSymbol;
 function generatePassword() {
-  var password = 'password';
+   var password = 'randomPassword';
   // TODO: add code to generate the password here
   var confirmLength = (prompt("How many characters would you like your password to contain?"));
     
-  while(confirmLength <= 7 || confirmLength >= 127) {
+  while(confirmNumber <= 7 || confirmNumber >= 127) {
     alert ("Password length must be between 8 and 126");
-    var confirmLength = (prompt("How many characters would you like your password to contain?"));
+    var confirmNumber= (prompt("How many characters would you like your password to contain?"));
     } 
     var confirmLower=(confirm("would you like to add lowercase letters"));
     console.log(confirmLower)
@@ -24,9 +24,24 @@ function generatePassword() {
     var confirmSymbols=(confirm("would you like to add special symbols"))
     console.log(confirmSymbols)
     
-  
-  return password;
-}
+    var passwordinputs=[]
+    if(confirmLower===true) {console.log("The user wants to add lower case")}
+    if(confirmLower===false) {console.log("The user does not want to add lower case")}
+    if(confirmUpper===true) {console.log("The user wants to add uppercase")}
+    if(confirmUpper===false) {console.log("The user does not want to add uppercase case")}
+    if(confirmSymbols===true) {console.log("The user wants to add symbols")}
+    if(confirmSymbols===false) {console.log("The user does not want to add symbols")}
+    
+    
+    var randomPassword=""
+    for (var i = 0; i < confirmLength; i++) {
+        randomPassword = randomPassword + passwordinputs[Math.floor(Math.random() * passwordinputs.length)];
+        console.log(randomPassword)}
+    
+    
+    return password;
+    
+} 
 
 // Write password to the #password input
 function writePassword() {
@@ -37,4 +52,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener('click', writePassword)
