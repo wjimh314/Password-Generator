b884443753201ae5bbcb33714b7d6d48dddee12e
wjimh314 <wjimh314@gmail.com>
2022-08-03 09:36:29 -0400

    edits

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var generateBtn = document.querySelector("#generate");
+
 var confirmLower = "abcdefghijklmnopqrstuvwxyz";
 var confirmUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var optionsNumber = "8-126";
@@ -10,47 +11,49 @@ var confirmLength = [
 	"confirmUpper",
 	"confirmSymbol",
 ];
-console.log(confirmLength);
+console.log(confirmLength);​
 var password = "randomPassword";
 // TODO: add code to generate the password here
-function pwd() {
-	var confirmLength = prompt(
+function pwd()
+{
+var confirmLength = prompt(
+	"How many characters would you like your password to contain?"
+);
+
+while (confirmNumber > 7 || confirmNumber > 127) {
+	alert("Password length must be between 8 and 126");
+	var confirmNumber = prompt(
 		"How many characters would you like your password to contain?"
 	);
-	while (confirmNumber > 7 || confirmNumber > 127) {
-		alert("Password length must be between 8 and 126");
-		var confirmNumber = prompt(
-			"How many characters would you like your password to contain?"
-		);
-	}
-	var confirmLowerC = confirm("would you like to add lowercase letters");
-	var confirmUpperC = confirm("would you like to add uppercase letters");
+}
+var confirmLowerC = confirm("would you like to add lowercase letters");
+var confirmUpperC = confirm("would you like to add uppercase letters");
+var confirmSymbolsC = confirm("would you like to add special symbols");
 
-	var confirmSymbolsC = confirm("would you like to add special symbols");
-	let passwordinputs = "";
-	if (confirmLowerC === true) {
-		console.log("The user wants to add lower case");
-		passwordinputs = passwordinputs.concat(confirmLower);
-	}
-	if (confirmLowerC === false) {
-		console.log("The user does not want to add lower case");
-	}
-	if (confirmUpperC === true) {
-		console.log("The user wants to add uppercase");
-		passwordinputs = passwordinputs.concat(confirmUpper);
-	}
-	if (confirmUpperC === false) {
-		console.log("The user does not want to add uppercase case");
-	}
-	if (confirmSymbolsC === true) {
-		console.log("The user wants to add symbols");
-		passwordinputs = passwordinputs.concat(ConfirmSymbols);
-	}
-	if (confirmSymbolsC === false) {
-		console.log("The user does not want to add symbols");
-	}
-	console.log(passwordinputs);
-	generatePassword(confirmLength, passwordinputs);
+let passwordinputs = "";
+if (confirmLowerC === true) {
+	console.log("The user wants to add lower case");
+	passwordinputs = passwordinputs.concat(confirmLower)
+}
+if (confirmLowerC === false) {
+	console.log("The user does not want to add lower case");
+}
+if (confirmUpperC === true) {
+	console.log("The user wants to add uppercase");
+	passwordinputs = passwordinputs.concat(confirmUpper)
+}
+if (confirmUpperC === false) {
+	console.log("The user does not want to add uppercase case");
+}
+if (confirmSymbolsC === true) {
+	console.log("The user wants to add symbols");
+	passwordinputs = passwordinputs.concat(ConfirmSymbols)
+}
+if (confirmSymbolsC === false) {
+	console.log("The user does not want to add symbols");
+}
+console.log(passwordinputs);
+ generatePassword(confirmLength, passwordinputs);
 }
 
 var generatePassword = (confirmlength, passwordinputs) => {
@@ -67,5 +70,4 @@ var generatePassword = (confirmlength, passwordinputs) => {
 	return password;
 };
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", pwd);
